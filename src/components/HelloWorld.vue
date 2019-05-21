2f69b5254517a6e2ac31bb2abc1d4aa65ee177a0
梁开钦 <liangkq@seentao.com>
2019-05-21 20:32:46 +0800

    first

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="page">
+   图标： <svg-icon icon-class="caiwuhesuanyewu1"></svg-icon>
+   <i class="iconfont icon-caiwuhesuanyewu1"></i>
+</div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+    name: 'name',
+    data () {
+        return {
+
+        };
+    },
+    computed: {
+
+    },
+    created () {
+    },
+    methods: {
+
+    }
+};
+</script>
+
+<style scoped lang="scss">
+    i {
+        font-size: 10px;
+        color: red;
+    }
+</style>
