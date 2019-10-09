9d8effa0c496fe71e2f3a8df9dc79323738cdf5b
liangkq <liangkq@seentao.com>
2019-10-09 15:45:48 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">111</router-link> |
+      <router-link to="/">11123</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
